ab2fece482e23a187b74a4d9aef0467d190d19c8
balagex <csongebalazs@gmail.com>
2024-01-19 21:08:44 +0100

    login

--- src/app/view/login/login.component.ts
+++ src/app/view/login/login.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { FireAuthService } from '../../fire-auth.service';
 import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-login',
     standalone: true,
-    imports: [ButtonModule, NgClass, FormsModule],
+    imports: [ButtonModule, NgClass, FormsModule, NgIf],
     templateUrl: './login.component.html',
     styleUrl: './login.component.scss'
 })
@@ -17,69 +17,50 @@ export class LoginComponent implements OnInit {
     public password: string = '';
     public loginname: string = '';
     public loading = false;
+    public loginError = false;
 
     constructor(private fireAuthService: FireAuthService, private router: Router) { }
 
     ngOnInit() {
 
-        // this.fireAuthService.login('csongebalazs@gmail.com', 'kacsafosx2')
-        //     .then((userCredential) => {
-
-        //         console.debug('LoginComponent - belépés sikeres', userCredential);
-        //         this.fireAuthService.getTokenPromise(userCredential).then(
-        //             (token: string) => {
-        //                 this.fireAuthService.setCredential(token, userCredential.user.email);
-        //                 this.router.navigate(['/akcioLista']);
-        //             }
-        //         );
-        //     })
-        //     .catch((error) => {
-        //         console.error('LoginComponent - FB SIGN IN ERROR', error);
-
-        //         this.fireAuthService.clearCredital();
-        //     });
     }
 
     keypress(event: KeyboardEvent): void {
-        if (event.keyCode === 13 && !this.loading) {
-            this.loginHandler();
+        console.debug('keypress ', event);
+        if (!this.loading) {
+            this.loginError = false;
+            if (event.keyCode === 13) {
+                this.loginHandler();
+            }
         }
+
     }
 
     loginHandler(): void {
 
         if (this.loginname && this.loginname.trim().length > 0 && this.password && this.password.trim().length > 0) {
             this.loading = true;
+            this.loginError = false;
             console.debug('LOGIN INDUL ', this.loginname, this.password);
 
-            // this.loginService.login(this.loginname, this.password).subscribe(
-            //     valasz => {
-            //         console.debug('LoginComponent - sikeres bejelentkezés.', valasz);
-            //         this.tokenStorageService.loginValaszMentese(valasz);
+            this.fireAuthService.login(this.loginname, this.password)
+                .then((userCredential) => {
+                    this.loading = false;
+                    console.debug('LoginComponent - A belépés sikeres. ', userCredential);
+                    this.fireAuthService.getTokenPromise(userCredential).then(
+                        (token: string) => {
+                            this.fireAuthService.setCredential(token, userCredential.user.email);
+                            this.router.navigate(['/akcioLista']);
+                        }
+                    );
+                })
+                .catch((error) => {
+                    this.loading = false;
+                    this.loginError = true;
+                    console.error('LoginComponent - Hiba történt a bejelentkezés során! ', error);
 
-            //         if (valasz && valasz.lathatoMenuElemek && valasz.lathatoMenuElemek.length > 0 && valasz.lathatoMenuElemek.findIndex(m => m.utvonal && m.utvonal === this.returnUrl) > -1) {
-            //             console.debug('LoginComponent - látható menühöz tartozik a return url...');
-            //         } else {
-            //             console.debug('LoginComponent - nem látható menühöz tartozik a return url!');
-            //             this.returnUrl = '/prot/welcome';
-            //         }
-            //         if (this.returnUrl === '/prot/welcome' && valasz && valasz.kezdoMenuElemId && valasz.lathatoMenuElemek && valasz.lathatoMenuElemek.length > 0) {
-            //             const kezdoMenuElem = valasz.lathatoMenuElemek.find(m => m.id === valasz.kezdoMenuElemId);
-            //             if (kezdoMenuElem && kezdoMenuElem.utvonal) {
-            //                 this.returnUrl = kezdoMenuElem.utvonal;
-            //                 console.debug('LoginComponent - keudő menüelem látható menühöz tartozik és nincs return url, így ide megyünk: ', kezdoMenuElem.utvonal);
-            //             }
-            //         }
-
-            //         this.loading = false;
-            //     },
-            //     lerror => {
-            //         console.error('LoginComponent - hiba a bejelentkezés során! ', lerror);
-            //         // Ez törli a session storage és a loginValasz infókat is.
-            //         this.tokenStorageService.clearStorage();
-            //         this.loading = false;
-            //     }
-            // );
+                    this.fireAuthService.clearCredital();
+                });
         }
     }
 
